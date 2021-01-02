84b9db7141029f54e5f17f3bf9c162eef9023bd6
Nickolai-Charniak <nickolai.charniak@gmail.com>
2021-01-02 23:56:36 +0300

    added asp.net core web api basic str

---
+++ AwesomeNet/AwesomeNet.Web/Controllers/Abstractions/BaseAppApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeNet.Web.Controllers.Abstractions
+{
+    [ApiController]
+    public class BaseAppApiController : ControllerBase
+    {
+        
+    }
+}
---
+++ AwesomeNet/AwesomeNet.Web/Controllers/Implementations/AwesomeRepoAppApiController.cs
@@ -0,0 +1,12 @@
+﻿using AwesomeNet.Web.Controllers.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeNet.Web.Controllers.Implementations
+{
+    [Route("api/awesome-repo")]
+    public class AwesomeRepoController : BaseAppApiController
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok($"{nameof(AwesomeRepoController)}");
+    }
+}
